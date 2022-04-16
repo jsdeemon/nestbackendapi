eb315d642edad71dd0c6f73fe9ffe96b1b0ebb1b
jsdeemon <jsdeemon@gmail.com>
2022-04-16 16:14:16 +0500

    added documentation for create rolde dto

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,5 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger"; 
 
 export class CreateRoleDto {
+    @ApiProperty({example: 'ADMIN', description: 'User role value'})
     readonly value: string;
+
+    @ApiProperty({example: 'Administrator', description: 'User role description'})
     readonly description: string;
 }
