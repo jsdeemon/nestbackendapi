41bbec2a74c62d72cd855579031a4e39705bcc8a
jsdeemon <jsdeemon@gmail.com>
2022-04-16 15:45:37 +0500

    added documentation for create role functionality

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,11 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { RolesService } from './roles.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Role } from './roles.model';
 
+
+@ApiTags('Roles')
 @Controller('roles')
 export class RolesController {
     constructor(private roleService: RolesService) {}
 
+    @ApiOperation({summary: 'Create role for user'})
+    @ApiResponse({status: 200, type:  Role})
     @Post()
     create(@Body() dto: CreateRoleDto) {
         return this.roleService.createRole(dto)
