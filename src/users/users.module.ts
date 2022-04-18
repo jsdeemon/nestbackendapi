2536bfb07ebf96a1011f395afd22c52b27562971
vkclonov <vkclonov@gmail.com>
2022-04-18 10:22:46 +0500

    added jwt auth guards for user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersController } from './users.controller';
 import { User } from './users.model';
@@ -6,16 +6,19 @@ import { Role } from '../roles/roles.model';
 import { UserRoles } from '../roles/user-roles.model';
 import { UsersService } from './users.service';
 import {RolesModule} from '../roles/roles.module'
+import {AuthModule} from '../auth/auth.module'
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
   imports: [
 SequelizeModule.forFeature([User, Role, UserRoles]), 
-  RolesModule
+  RolesModule,
+  forwardRef(() => AuthModule),
   ], 
   exports: [
-    UsersService
+    UsersService,
+   
   ]
 })
 export class UsersModule {}
